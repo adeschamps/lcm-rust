c4bce035681413c50976634ce4a6067bc71e7f7c
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2018-02-20 17:59:54 -0500

    Add a few more tests for hash values.

--- tests/src/hashes.rs
+++ tests/src/hashes.rs
@@ -2,5 +2,11 @@ use lcm::Message;
 
 #[test]
 fn hashes() {
+    // Expected hash values were generated manually from the C
+    // implementation of lcm-gen.
+    assert_eq!(::member_group::HASH, 0xae7e5fba5eeca11e);
+    assert_eq!(::my_constants_t::HASH, 0x000000002468acf0);
+    assert_eq!(::my_struct_t::HASH, 0x4fab8e09620e9ec9);
+    assert_eq!(::point2d_list_t::HASH, 0x4f85d1e7da2fc594);
     assert_eq!(::temperature_t::HASH, 0xa07fa3d64cbea6ea);
 }
